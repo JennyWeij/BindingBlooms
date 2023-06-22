6e0255d08ff8a5473b2f2af7c3e4918b5d4509b9
JennyWeij <jennyweij@outlook.com>
2023-06-22 21:23:49 +0200

    nu scrollas användaren till our flowers delen av sidan om our flowers klickas på i headern

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
+import { Link } from "react-scroll";
 
 function Header() {
   return (
@@ -15,65 +16,92 @@ function Header() {
       <Flex align="center" justify="space-between">
         <Image src="/images/Logotype.png" alt="Logo" height="90px" ml={10} />
         <Flex>
-          <Box
-            as="button"
-            height="24px"
-            lineHeight="1.2"
-            transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-            border="1px"
-            px="8px"
-            borderRadius="2px"
-            fontSize="20px"
-            fontWeight="semibold"
-            fontFamily="Baskerville"
-            mr={20}
-            bg="none"
-            color="#B28767"
-            cursor="pointer"
-            _hover={{ bg: "none" }}
+          <Link
+            activeClass="active"
+            to="our-flowers-section"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
           >
-            Our flowers
-          </Box>
+            <Box
+              as="button"
+              height="24px"
+              lineHeight="1.2"
+              transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+              border="1px"
+              px="8px"
+              borderRadius="2px"
+              fontSize="20px"
+              fontWeight="semibold"
+              fontFamily="Baskerville"
+              mr={20}
+              bg="none"
+              color="#B28767"
+              cursor="pointer"
+              _hover={{ bg: "none" }}
+            >
+              Our flowers
+            </Box>
+          </Link>
 
-          <Box
-            as="button"
-            height="24px"
-            lineHeight="1.2"
-            transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-            border="1px"
-            px="8px"
-            borderRadius="2px"
-            fontSize="20px"
-            mr={20}
-            fontWeight="semibold"
-            bg="none"
-            color="#B28767"
-            fontFamily="Baskerville"
-            cursor="pointer"
-            _hover={{ bg: "none" }}
+          <Link
+            activeClass="active"
+            to="our-flowers-section"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
           >
-            Our courses
-          </Box>
+            <Box
+              as="button"
+              height="24px"
+              lineHeight="1.2"
+              transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+              border="1px"
+              px="8px"
+              borderRadius="2px"
+              fontSize="20px"
+              mr={20}
+              fontWeight="semibold"
+              bg="none"
+              color="#B28767"
+              fontFamily="Baskerville"
+              cursor="pointer"
+              _hover={{ bg: "none" }}
+            >
+              Our courses
+            </Box>
+          </Link>
 
-          <Box
-            as="button"
-            height="24px"
-            lineHeight="1.2"
-            transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-            border="1px"
-            px="8px"
-            borderRadius="2px"
-            fontSize="20px"
-            fontWeight="semibold"
-            bg="none"
-            mr={80}
-            color="#B28767"
-            fontFamily="Baskerville"
-            cursor="pointer"
-            _hover={{ bg: "none" }}
+          <Link
+            activeClass="active"
+            to="about-us-section"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
           >
-            About us
-          </Box>
+            <Box
+              as="button"
+              height="24px"
+              lineHeight="1.2"
+              transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+              border="1px"
+              px="8px"
+              borderRadius="2px"
+              fontSize="20px"
+              fontWeight="semibold"
+              bg="none"
+              mr={80}
+              color="#B28767"
+              fontFamily="Baskerville"
+              cursor="pointer"
+              _hover={{ bg: "none" }}
+            >
+              About us
+            </Box>
+          </Link>
         </Flex>
       </Flex>
     </Box>
--- src/pages/FlowersPage.tsx
+++ src/pages/FlowersPage.tsx
@@ -1,86 +1,90 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import { Element } from "react-scroll";
 
 function FlowersPage() {
   return (
     <>
-      <Flex
-        bg="linear-gradient(to right, #000E19, #192B36)"
-        justifyContent="center"
-        alignItems="center"
-        style={{ width: "100%", height: "100vh" }}
-      >
-        <img
-          src="/images/flowerfield.jpg"
-          alt="Background Image"
-          style={{
-            width: "100%",
-            height: "100vh",
-            objectFit: "cover",
-            marginTop: "100px",
-          }}
-        />
-        <Box
-          position="absolute"
-          textAlign="center"
-          fontSize="100px"
-          color="#B28767"
-          fontWeight="bold"
-          fontFamily="Baskerville"
+      <Element name="our-flowers-section">
+        <Flex
+          bg="linear-gradient(to right, #000E19, #192B36)"
+          justifyContent="center"
+          alignItems="center"
+          style={{ width: "100%", height: "100vh" }}
         >
-          Our flowers
-        </Box>
-      </Flex>
-
-      <Flex pl={90} pt={100} bg="linear-gradient(to right, #000E19, #192B36)">
-        <Box style={{ width: "400px", height: "500px" }}>
           <img
-            src="/images/fieldsunset.jpg"
+            src="/images/flowerfield.jpg"
             alt="Background Image"
-            style={{ width: "400px", height: "100%", objectFit: "cover" }}
+            style={{
+              width: "100%",
+              height: "100vh",
+              objectFit: "cover",
+              marginTop: "100px",
+            }}
           />
-        </Box>
-        <Box p={2} flex="1">
-          <Text
-            pr={150}
-            pl={20}
-            fontSize="18px"
-            fontFamily="SourceSansRegular"
+          <Box
+            position="absolute"
+            textAlign="center"
+            fontSize="100px"
             color="#B28767"
+            fontWeight="bold"
+            fontFamily="Baskerville"
           >
-            At our binding bouquets company, we take pride in the exceptional
-            quality of our flowers. We meticulously grow and nurture them in our
-            very own flower fields, located in picturesque countryside regions.
-            Our flower fields are carefully chosen to provide the optimal
-            conditions for each flower variety to thrive. We cultivate a diverse
-            range of certified flowers, ensuring that each bloom meets our
-            stringent standards of excellence. From the moment our seeds are
-            sown, our dedicated team of horticulturists and gardeners
-            attentively tend to the flowers, providing them with the ideal
-            environment to grow and flourish. We prioritize sustainable and
-            eco-friendly farming practices, using organic fertilizers and
-            natural pest control methods to minimize any impact on the
-            environment. Harvesting is a meticulous process that requires a keen
-            eye for each flower's peak perfection. Our experienced floral
-            experts carefully handpick each stem at the precise moment when its
-            colors are vibrant and its petals are in full bloom. This ensures
-            that our customers receive only the freshest and most stunning
-            flowers. Once harvested, our flowers are quickly transported to our
-            company's facilities, where they undergo a rigorous quality control
-            process. Our expert florists meticulously inspect each stem,
-            ensuring that only the finest and healthiest flowers make it into
-            our bouquets. To preserve their freshness, our flowers are stored in
-            climate-controlled environments. We employ innovative techniques,
-            such as cold chain management, to maintain optimal temperature and
-            humidity levels during transportation, ensuring that our bouquets
-            arrive at their destination in perfect condition. By nurturing our
-            flowers from seed to bouquet, we guarantee the utmost care and
-            attention to detail in every step of the process. Our certified
-            flowers embody the beauty of nature and serve as a testament to our
-            commitment to delivering exceptional floral arrangements that exceed
-            expectations.
-          </Text>
-        </Box>
-      </Flex>
+            Our flowers
+          </Box>
+        </Flex>
+
+        <Flex pl={90} pt={100} bg="linear-gradient(to right, #000E19, #192B36)">
+          <Box style={{ width: "400px", height: "500px" }}>
+            <img
+              src="/images/fieldsunset.jpg"
+              alt="Background Image"
+              style={{ width: "400px", height: "100%", objectFit: "cover" }}
+            />
+          </Box>
+          <Box p={2} flex="1">
+            <Text
+              pr={150}
+              pl={20}
+              fontSize="18px"
+              fontFamily="SourceSansRegular"
+              color="#B28767"
+            >
+              At our binding bouquets company, we take pride in the exceptional
+              quality of our flowers. We meticulously grow and nurture them in
+              our very own flower fields, located in picturesque countryside
+              regions. Our flower fields are carefully chosen to provide the
+              optimal conditions for each flower variety to thrive. We cultivate
+              a diverse range of certified flowers, ensuring that each bloom
+              meets our stringent standards of excellence. From the moment our
+              seeds are sown, our dedicated team of horticulturists and
+              gardeners attentively tend to the flowers, providing them with the
+              ideal environment to grow and flourish. We prioritize sustainable
+              and eco-friendly farming practices, using organic fertilizers and
+              natural pest control methods to minimize any impact on the
+              environment. Harvesting is a meticulous process that requires a
+              keen eye for each flower's peak perfection. Our experienced floral
+              experts carefully handpick each stem at the precise moment when
+              its colors are vibrant and its petals are in full bloom. This
+              ensures that our customers receive only the freshest and most
+              stunning flowers. Once harvested, our flowers are quickly
+              transported to our company's facilities, where they undergo a
+              rigorous quality control process. Our expert florists meticulously
+              inspect each stem, ensuring that only the finest and healthiest
+              flowers make it into our bouquets. To preserve their freshness,
+              our flowers are stored in climate-controlled environments. We
+              employ innovative techniques, such as cold chain management, to
+              maintain optimal temperature and humidity levels during
+              transportation, ensuring that our bouquets arrive at their
+              destination in perfect condition. By nurturing our flowers from
+              seed to bouquet, we guarantee the utmost care and attention to
+              detail in every step of the process. Our certified flowers embody
+              the beauty of nature and serve as a testament to our commitment to
+              delivering exceptional floral arrangements that exceed
+              expectations.
+            </Text>
+          </Box>
+        </Flex>
+      </Element>
     </>
   );
 }
