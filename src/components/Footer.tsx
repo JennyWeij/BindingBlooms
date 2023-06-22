89dd5d286e69cdb38caadffca8e74db4b29e30f6
JennyWeij <jennyweij@outlook.com>
2023-06-22 23:14:08 +0200

    lagt till grid i footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,41 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, Text } from "@chakra-ui/react";
 import { GrFacebook, GrInstagram, GrTwitter } from "react-icons/gr";
 
 function Footer() {
   return (
-    <>
-      <Box
-        bg="linear-gradient(to right, #000E19, #192B36)"
-        h="300px"
-        bottom="0"
-        pt="50px"
-        width="100vw"
+    <Box
+      bg="linear-gradient(to right, #000E19, #192B36)"
+      h="300px"
+      bottom="0"
+      pt="50px"
+      width="100vw"
+    >
+      <Grid
+        templateColumns="1fr 1fr 1fr" // Create three equal-width columns
+        gap={0} // Add a gap between columns
+        maxW="800px" // Set a maximum width for the grid
+        mx="auto" // Center the grid horizontally
+        alignItems="center" // Center the items vertically within each cell
       >
-        <Box style={{ width: "400px", height: "250px" }}>
+        <Box gridColumn="1">
+          <Text color="#B28767" fontSize="24px" fontWeight="bold">
+            Binding Blooms
+          </Text>
+        </Box>
+        <Box gridColumn="2">
+          <GrFacebook size={24} color="#B28767" />
+          <GrInstagram size={24} color="#B28767" />
+          <GrTwitter size={24} color="#B28767" />
+        </Box>
+        <Box gridColumn="3">
           <img
             src="/images/blommor.jpg"
             alt="Flowers in wooden basket"
-            style={{ width: "400px", height: "100%", objectFit: "contain" }}
+            style={{ width: "150px", height: "100%", objectFit: "contain" }}
           />
         </Box>
-        <Box>
-          <GrFacebook size={24} color="#B28767" />
-        </Box>
-        <Box>
-          <GrInstagram size={24} color="#B28767" />
-        </Box>
-        <Box>
-          <GrTwitter size={24} color="#B28767" />
-        </Box>
-      </Box>
-    </>
+      </Grid>
+    </Box>
   );
 }
 
