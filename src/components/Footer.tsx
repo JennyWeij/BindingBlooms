34458d114206517adaf9dfb605e766732ed07fea
JennyWeij <jennyweij@outlook.com>
2023-06-22 23:23:47 +0200

    uppdaterat footern

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Text } from "@chakra-ui/react";
+import { Box, Grid, Image, Text } from "@chakra-ui/react";
 import { GrFacebook, GrInstagram, GrTwitter } from "react-icons/gr";
 
 function Footer() {
@@ -11,11 +11,11 @@ function Footer() {
       width="100vw"
     >
       <Grid
-        templateColumns="1fr 1fr 1fr" // Create three equal-width columns
-        gap={0} // Add a gap between columns
-        maxW="800px" // Set a maximum width for the grid
-        mx="auto" // Center the grid horizontally
-        alignItems="center" // Center the items vertically within each cell
+        templateColumns="1fr 1fr 1fr"
+        gap={0}
+        maxW="1000px"
+        mx="auto"
+        alignItems="center"
       >
         <Box gridColumn="1">
           <Text color="#B28767" fontSize="24px" fontWeight="bold">
@@ -23,15 +23,28 @@ function Footer() {
           </Text>
         </Box>
         <Box gridColumn="2">
-          <GrFacebook size={24} color="#B28767" />
-          <GrInstagram size={24} color="#B28767" />
-          <GrTwitter size={24} color="#B28767" />
+          <Box display="flex" alignItems="center">
+            <GrFacebook size={23} color="#B28767" />
+            <Box marginLeft={20}>
+              <GrInstagram size={24} color="#B28767" />
+            </Box>
+            <Box marginLeft={20}>
+              <GrTwitter size={24} color="#B28767" />
+            </Box>
+          </Box>
         </Box>
-        <Box gridColumn="3">
+        <Box gridColumn="3" alignItems="self-end">
           <img
             src="/images/blommor.jpg"
             alt="Flowers in wooden basket"
             style={{ width: "150px", height: "100%", objectFit: "contain" }}
+          />
+          <Image
+            src="/images/Logotype.png"
+            alt="Logo"
+            height="70px"
+            mt={0}
+            ml={50}
           />
         </Box>
       </Grid>
